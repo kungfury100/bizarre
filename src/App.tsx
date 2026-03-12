e59702ac70cccb9ec57bca6569ffdddb91ceee60
Karl <52241072+kungfury100@users.noreply.github.com>
2026-03-12 15:36:12 +0200

    Add Rive & Tailwind; update favicon/title

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Rive from '@rive-app/react-webgl2'
 import './App.css'
+import hologramRiv from './assets/hologram.riv?url'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='flex flex-col items-center'>
+      <Rive
+      className='hero-rive'
+        src={hologramRiv}
+        stateMachines="Play"
+      />
+      <h1>Hello there explorer.</h1>
+      <p>My name is Karl, I do product design and web development. On my journey, I've led design process for user-centric products and design systems.</p>
+      <p>(Been designing for two <a href="https://www.lift99.co/walloffame" target="_blank" rel="noopener noreferrer">#EstonianMafia</a> startups.)</p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button>Check out my work</button>
+        <button>Read my CV</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
