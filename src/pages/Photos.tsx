100e3226d79476532dadc8ff8505a0028f3a57bf
Karl <52241072+kungfury100@users.noreply.github.com>
2026-03-19 09:30:35 +0200

    Fix Navbar responsive issues

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,6 +1,7 @@
 function Photos() {
   return (
     <div>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br />
       <p>Films are in development, please come back tomorrow.</p>
     </div>
   )
